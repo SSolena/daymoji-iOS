8865bc4eca146ba49586202f2a81209407dbda07
dev-Lena <keunnalee@gmail.com>
2021-03-13 20:08:15 +0900

    chore: initial commit

---
+++ Daymoji/Daymoji/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Daymoji
+//
+//  Created by Keunna Lee on 2021/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ Daymoji/Daymoji/DaymojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaymojiApp.swift
+//  Daymoji
+//
+//  Created by Keunna Lee on 2021/03/13.
+//
+
+import SwiftUI
+
+@main
+struct DaymojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
