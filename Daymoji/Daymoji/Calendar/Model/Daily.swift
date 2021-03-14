377502a75256abcad1898661810f8e85c7db9ece
dev-Lena <keunnalee@gmail.com>
2021-03-14 23:42:56 +0900

    feat: initial setting for Calendar

---
+++ Daymoji/Daymoji/Calendar/Model/Daily.swift
@@ -0,0 +1,18 @@
+//
+//  Daily.swift
+//  Daymoji
+//
+//  Created by Keunna Lee on 2021/03/14.
+//
+
+import Foundation
+
+struct Daily {
+    var date: Date
+    var emojiImageURL: String
+    var title: String
+    var address: String
+    var isSharedOn: Bool
+    var totalReaction: String
+
+}
---
+++ Daymoji/Daymoji/Calendar/View/CalendarView.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarView.swift
+//  Daymoji
+//
+//  Created by Keunna Lee on 2021/03/14.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
