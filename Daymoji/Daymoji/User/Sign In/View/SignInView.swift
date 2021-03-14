a35b1ef7a01a3fa5da8761c0cde0b0dea13a8614
dev-Lena <keunnalee@gmail.com>
2021-03-14 23:43:52 +0900

    feat: initial setting for User

---
+++ Daymoji/Daymoji/User/Sign In/View/SignInView.swift
@@ -0,0 +1,15 @@
+//
+//  SignInView.swift
+//  Daymoji
+//
+//  Created by Keunna Lee on 2021/03/14.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
